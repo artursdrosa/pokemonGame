84f9335aa3f765c1ebb9b4ca600891aaca2a5d96
artursdrosa <artursr22@hotmail.com>
2024-06-24 22:07:06 -0300

    creating class: Obstacle, EnemyPokemon, Item, ObstacleFactory

---
+++ src/EnemyPokemon.java
@@ -0,0 +1,24 @@
+public class EnemyPokemon extends Obstacle {
+    int heathPoints;
+    int attackPoints;
+
+    public int getHeathPoints() {
+        return heathPoints;
+    }
+
+    public int getAttackPoints() {
+        return attackPoints;
+    }
+    public void setHeathPoints(int heathPoints) {
+        this.heathPoints = heathPoints;
+    }
+
+    public void setAttackPoints(int attackPoints) {
+        this.attackPoints = attackPoints;
+    }
+
+    public void attack(Pokemon pokemon){
+        //pokemon.vida -= attackPoints
+    }
+
+}
---
+++ src/Item.java
@@ -0,0 +1,8 @@
+public class Item extends Obstacle{
+    boolean type;   //false = power, true = health
+    int points;
+
+    public void use(Pokemon pokemon, Boolean tipo){
+
+    }
+}
