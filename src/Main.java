b16586da381c12c043be47b2c5d276deab31cd5a
artursdrosa <artursr22@hotmail.com>
2024-07-01 22:47:28 -0300

    iniciating the selection of the pokemons

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,18 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("You started your Pokemon Journey!\n");
 		Player theo = new Player("theo");
+		Scanner sc = new Scanner(System.in);
+		int qtdPokemon = 0;
+		while (qtdPokemon != 2) { // Forces the player to have 2 pokemons
+			int optPokemon = 0;
+			while(optPokemon !=3 || optPokemon!=2 || optPokemon!=1) {
+				System.out.println("Choose your pokemon:\n"+);
+				//tem que fazer uma forma com que eu consiga acessar as infos dos pokemons aqui cpa, ou talvez só digite o nome e as stats dele na mao mesmo fdc
+			}
+
+		}
 		Path p = Path.getInstance();
 		p.startOrResetGame(6, theo);
 		//System.out.println(p.toString());
